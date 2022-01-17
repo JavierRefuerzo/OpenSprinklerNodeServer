fa5caedbc9747061d7a5e7a3b6e1bcb5ec42f5df
Javier Refuerzo <javierrefuerzo@gmail.com>
2022-01-16 17:28:14 -0700

    removed open sprinkler lib

--- .open_sprinkler/src/python-opensprinkler/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='python-opensprinkler',
-    version='0.2.0',
-    packages=['opensprinkler',],
-    license='New BSD License',
-    long_description=open('README.rst').read(),
-)
